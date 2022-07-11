37bc10f2adda959d6650d777caf4c8328881c7d2
pixee <help@pixee.ai>
2022-07-11 20:57:14 +0000

    Updated EncodingAssignment.java:52 - Replaced the weak pseudo-random number generator with a strong one
    [Rules]
     - pixee:java/secure-random

--- src/main/java/org/owasp/webgoat/lessons/cryptography/EncodingAssignment.java
+++ src/main/java/org/owasp/webgoat/lessons/cryptography/EncodingAssignment.java
@@ -49,7 +49,7 @@ public class EncodingAssignment extends AssignmentEndpoint {
 		String basicAuth = (String) request.getSession().getAttribute("basicAuth");
 		String username = request.getUserPrincipal().getName();
 		if (basicAuth == null) {
-			String password = HashingAssignment.SECRETS[new Random().nextInt(HashingAssignment.SECRETS.length)];
+			String password = HashingAssignment.SECRETS[new java.security.SecureRandom().nextInt(HashingAssignment.SECRETS.length)];
 			basicAuth = getBasicAuth(username, password);
 			request.getSession().setAttribute("basicAuth", basicAuth);
 		}
