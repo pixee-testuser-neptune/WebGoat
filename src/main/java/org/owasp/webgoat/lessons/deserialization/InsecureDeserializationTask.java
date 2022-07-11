bcddddf8e55962f22ddf157cdc2edadc2fe4c760
pixee <help@pixee.ai>
2022-07-11 06:19:51 +0000

    Updated InsecureDeserializationTask.java:56 - Hardened the deserialization call by introducing a filter that prevents known malicious gadgets from executing arbitrary code
    [Rules]
     - pixee:java/harden-java-deserialization

--- src/main/java/org/owasp/webgoat/lessons/deserialization/InsecureDeserializationTask.java
+++ src/main/java/org/owasp/webgoat/lessons/deserialization/InsecureDeserializationTask.java
@@ -53,6 +53,7 @@ public class InsecureDeserializationTask extends AssignmentEndpoint {
 
         try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(Base64.getDecoder().decode(b64token)))) {
             before = System.currentTimeMillis();
+            io.pixee.security.Deserialization.enableObjectFilterIfUnprotected(ois);
             Object o = ois.readObject();
             if (!(o instanceof VulnerableTaskHolder)) {
                 if (o instanceof String) {
