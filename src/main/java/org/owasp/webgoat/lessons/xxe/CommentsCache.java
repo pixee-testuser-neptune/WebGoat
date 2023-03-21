aabc547b1475cd7a0124e4edf08f280cfae9a32d
pixee <help@pixee.ai>
2023-03-21 13:47:43 +0000

    Updated CommentsCache.java:96 - Hardened the XML processor to prevent external entities from being resolved, which can prevent data exfiltration and arbitrary code execution
    [Rules]
     - pixee:java/harden-xmlinputfactory

--- src/main/java/org/owasp/webgoat/lessons/xxe/CommentsCache.java
+++ src/main/java/org/owasp/webgoat/lessons/xxe/CommentsCache.java
@@ -23,6 +23,7 @@
 package org.owasp.webgoat.lessons.xxe;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.openpixee.security.XMLInputFactorySecurity;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
@@ -93,7 +94,7 @@ public class CommentsCache {
      */
     protected Comment parseXml(String xml) throws JAXBException, XMLStreamException {
         var jc = JAXBContext.newInstance(Comment.class);
-        var xif = XMLInputFactory.newInstance();
+        var xif = XMLInputFactorySecurity.hardenFactory(XMLInputFactory.newInstance());
 
         if (webSession.isSecurityEnabled()) {
             xif.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, ""); // Compliant
